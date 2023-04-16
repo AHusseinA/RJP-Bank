e57e0a5e451e6d270d8c03a39a59a311bc132847
AHusseinA <hussein.hammoud.90@hotmail.com>
2023-04-16 17:37:20 +0300

    Revert "Create Data, Service and TransactionMicroservice Projects"
    This reverts commit 15e56469554ef489ec63e31fc0066170039fd06a.

--- Transaction.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Transaction.Data
-{
-    public class Class1
-    {
-    }
-}
--- Transaction.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Transaction.Service
-{
-    public class Class1
-    {
-    }
-}
