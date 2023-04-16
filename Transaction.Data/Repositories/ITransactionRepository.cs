e2b36fa701ccb99efdd9beb528a8d7e85460cdd9
AHusseinA <hussein.hammoud.90@hotmail.com>
2023-04-16 17:14:03 +0300

    Transaction Microservice Business

---
+++ Transaction.Data/Repositories/ITransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction.Data
+{
+    public interface ITransactionRepository
+    {
+        Task CreateTransactionAsync(Transaction transaction);
+        Task<Transaction> GetTransactionAsync(int transactionId);
+        Task<List<Transaction>> GetTransactionsForAccountAsync(int accountId);
+    }
+}
---
+++ Transaction.Service/ITransactionService.cs
@@ -0,0 +1,15 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction.Service
+{
+    public interface ITransactionService
+    {
+        Task<TransactionInfo> CreateTransactionAsync(TransactionInfo transactionInfo);
+        Task<List<TransactionInfo>> GetTransactionsForAccountAsync(int accountId);
+    }
+}
